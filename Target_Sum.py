b602ee3caf5db1830abb3a52f908344204b89f97
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-16 17:07:10 +0800

    pass testcase: testSampleCase by using queue

--- Target_Sum.py
+++ Target_Sum.py
@@ -4,11 +4,29 @@ class unitest(unittest.TestCase):
     def testEmptylist(self):
         nums = []
         S = 0
-        self.assertEqual(Solution().findTargetSumWays(nums,S),0);
+        self.assertEqual(Solution().findTargetSumWays(nums,S),0)
+    def testSampleCase(self):
+        nums = [1,1,1,1,1]
+        S = 3
+        self.assertEqual(Solution().findTargetSumWays(nums,S),5)
 
 class Solution():
     def findTargetSumWays(self, nums, S):
         ans = 0
+        if nums == []:
+            return ans
+        queue = [0,'E']
+        i = 0
+        for node in queue:
+            if i == len(nums):
+                if node == S:
+                    ans += 1
+                continue
+            if node == 'E':
+                i += 1
+                queue += 'E'
+            else:
+                queue += ((node + nums[i]),(node - nums[i]))
         return ans
 
 if __name__ == '__main__':
