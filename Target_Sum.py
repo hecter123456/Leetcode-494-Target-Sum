c5485e4d92d2bb2b6d2424dc021d0ff5b0c2fbb4
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-24 16:41:40 +0800

    pass testcase: TLEcase by using defaultdict

--- Target_Sum.py
+++ Target_Sum.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import defaultdict
 
 class unitest(unittest.TestCase):
     def testEmptylist(self):
@@ -9,25 +10,23 @@ class unitest(unittest.TestCase):
         nums = [1,1,1,1,1]
         S = 3
         self.assertEqual(Solution().findTargetSumWays(nums,S),5)
+    def testTLEcase(self):
+        nums = [10,9,6,4,19,0,41,30,27,15,14,39,33,7,34,17,24,46,2,46]
+        S = 45
+        self.assertEqual(Solution().findTargetSumWays(nums,S),6606)
 
 class Solution():
     def findTargetSumWays(self, nums, S):
-        ans = 0
-        if nums == []:
-            return ans
-        queue = [0,'E']
-        i = 0
-        for node in queue:
-            if i == len(nums):
-                if node == S:
-                    ans += 1
-                continue
-            if node == 'E':
-                i += 1
-                queue += 'E'
-            else:
-                queue += ((node + nums[i]),(node - nums[i]))
-        return ans
+        if not nums:
+            return 0
+        Ans = {0:1}
+        for node in nums:
+            dic = defaultdict(int)
+            for key,value in Ans.items():
+                dic[key + node] += value
+                dic[key - node] += value
+            Ans = dic
+        return Ans[S]
 
 if __name__ == '__main__':
     unittest.main()
